f3669d16f03d39e27497ee078eb9d119bfc5518f
huytuong010101 <huytuong010101@gmail.com>
2021-04-26 09:30:02 +0700

    structure +  UI checking

--- HeThongGiuXe/Program.cs
+++ HeThongGiuXe/Program.cs
@@ -16,7 +16,7 @@ namespace HeThongGiuXe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new View.CheckinForm());
         }
     }
 }
