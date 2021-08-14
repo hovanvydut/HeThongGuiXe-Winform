ce198261c00320d93b21e445ce809f5b234f4c59
tuongnh <huytuong010101@gmail.com>
2021-08-14 15:18:35 +0700

    fix bug employee form when start

--- HeThongGiuXe/Program.cs
+++ HeThongGiuXe/Program.cs
@@ -17,7 +17,7 @@ namespace HeThongGiuXe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManageEmployeeForm());
+            Application.Run(new MainForm());
             //Application.Run(new AnalysisRevenueForm());
             //Application.Run(new PaymentManagementForm());
             //Application.Run(new ParkingPackageForm());
